0a2c1f9b47b503f7a0dbed335a8964bf4d890bcc
KsaweryBartman <122223184+KsaweryBartman@users.noreply.github.com>
2023-01-10 23:46:43 +0100

    [src] focused on detecting a c compiler for now

--- src/main.c
+++ src/main.c
@@ -1,45 +1,65 @@
-#include <stddef.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include "cmd.h"
 
-#define DEF_MIN_MAX_FOR_TYPE(t)                                                \
-  t t##_max(t a, t b) {                                                        \
-    if (a > b)                                                                 \
-      return a;                                                                \
-    return b;                                                                  \
-  }                                                                            \
-  t t##_min(t a, t b) {                                                        \
-    if (a < b)                                                                 \
-      return a;                                                                \
-    return b;                                                                  \
+const char *CMD_DISCARD_OUTPUT = " >/dev/null 2>/dev/null";
+
+const char *detect_cmd(const char *cmdlist[], size_t cmdlist_len) {
+  for (size_t i = 0; i < cmdlist_len; i++) {
+    char cmd[255] = {};
+    strcpy(cmd, cmdlist[i]);
+    strcat(cmd, " -v");
+    strcat(cmd, CMD_DISCARD_OUTPUT);
+    int result = system(cmd);
+    if (result == 0) {
+      return cmdlist[i];
+    }
+  }
+  return NULL;
+}
+
+int main(int arg_count, char **arg_array) {
+  // Parse all arguments
+  struct cmd_arg *args = parse_args(arg_count, arg_array);
+
+  char *c_compiler = get_arg(arg_count, args, "CC");
+  char *cpp_compiler = get_arg(arg_count, args, "CXX");
+  printf(
+      "\n[unibuild]\n\tDetected C Compiler: %s\n\tDetected C++ Compiler: %s\n",
+      c_compiler, cpp_compiler);
+  if (c_compiler == NULL) {
+    const char *c_compilers[] = {
+        "cl",
+        "clang",
+        "gcc",
+    };
+    const size_t c_compilers_size = sizeof(c_compilers) / sizeof(const char *);
+    c_compiler = detect_cmd(c_compilers, c_compilers_size);
+    if (c_compiler == NULL) {
+      printf("[unibuild error]\nCouldn't automatically detect a C "
+             "compiler.\nPlease "
+             "define it by hand.\n\tunibuild CC=\"/path/to/c_compiler\"\n");
+      return 1;
+    }
+  }
+  if (cpp_compiler == NULL) {
+    const char *cpp_compilers[] = {
+        "cl",
+        // "clang++",
+        // "g++",
+    };
+    const size_t cpp_compilers_size =
+        sizeof(cpp_compilers) / sizeof(const char *);
+    cpp_compiler = detect_cmd(cpp_compilers, cpp_compilers_size);
+    if (cpp_compiler == NULL) {
+      printf("[unibuild error]\nCouldn't automatically detect a C++ "
+             "compiler.\nPlease "
+             "define it by hand.\n\tunibuild CXX=/path/to/cpp_compiler\n");
+      return 2;
+    }
   }
 
-DEF_MIN_MAX_FOR_TYPE(size_t);
-
-int main(int argc, char **argv) {
-  printf("[unibuild] Hello, Man!\n");
-
-  // Compiler detection
-  const char *c_compiler = "gcc";
-  const char *cpp_compiler = "g++";
-  const char *source_files = "src/main.c";
-  const char *output_files = "build/unibuild";
-
-  char *command;
-
-  size_t command_size = size_t_max(strlen(c_compiler), strlen(cpp_compiler)) +
-                        strlen(source_files) + strlen(output_files) +
-                        strlen("  -o ") + 1;
-  command = malloc(command_size);
-  memset(command, 0, command_size);
-  strcpy(command, c_compiler);
-  strcpy(command + strlen(command), " ");
-  strcpy(command + strlen(command), source_files);
-  strcpy(command + strlen(command), " -o ");
-  strcpy(command + strlen(command), output_files);
-  // printf("%s\n", command);
-  system(command);
+  printf(
+      "\n[unibuild]\n\tDetected C Compiler: %s\n\tDetected C++ Compiler: %s\n",
+      c_compiler, cpp_compiler);
 
   return 0;
 }
